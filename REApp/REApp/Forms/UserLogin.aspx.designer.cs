103d42a81053587c5c744ecd4331d6604f07f141
BenjaLeonardi <benjaleonardi1998@hotmail.com>
2022-07-14 11:08:18 -0300

    Validaciones Iniciales
    Testeo y capacitacion

--- REApp/REApp/Forms/UserLogin.aspx.designer.cs
+++ REApp/REApp/Forms/UserLogin.aspx.designer.cs
@@ -13,5 +13,41 @@ namespace REApp.Forms
 
     public partial class UserLogin
     {
+
+        /// <summary>
+        /// Control FormLogin.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm FormLogin;
+
+        /// <summary>
+        /// Control txt_email.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl txt_email;
+
+        /// <summary>
+        /// Control txt_password.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputPassword txt_password;
+
+        /// <summary>
+        /// Control login.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button login;
     }
 }
